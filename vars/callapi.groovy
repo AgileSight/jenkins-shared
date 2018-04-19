7328836d598bbffdf00d2eab9cebc8c1b2c52a8b
Pablo Cibraro <pcibraro@hotmail.com>
2018-04-19 16:35:05 -0300

    new

--- vars/callapi.groovy
+++ vars/callapi.groovy
@@ -5,3 +5,17 @@ def callme(message) {
 
     return responseContent
 }
+
+def buildme(artifactory) {
+    def server = artifactory.server 'local'
+    
+    def maven = artifactory.newMavenBuild()
+
+    maven.resolver server: server, releaseRepo: 'jcenter', snapshotRepo: 'jcenter'
+
+    maven.deployer server: server, releaseRepo: 'samples', snapshotRepo: 'samples'
+
+    maven.deployer.deployArtifacts = true
+
+    def buildInfo = maven.run pom: './my-app/pom.xml', goals: 'clean package'
+}
